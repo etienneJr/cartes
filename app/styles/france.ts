76002c837edde6bb68d4d2b591354e3e7fd58380
Mael <laem@kont.me>
2024-09-10 14:47:16 +0200

    Ajout naïf des contours en copiant depuis le style nature

--- app/styles/france.ts
+++ app/styles/france.ts
@@ -54,6 +54,10 @@ export default function franceStyle(transportMode, noVariableTiles = false) {
 				minzoom: 2,
 				maxzoom: 12,
 			},
+			contours: {
+				type: 'vector',
+				url: 'pmtiles://' + pmtilesServerUrl + '/contour-lines.pmtiles',
+			},
 		},
 		layers: transportMode ? lightenLayers(layers) : layers,
 		//		Voir nos villes juste avec les arbres
@@ -137,6 +141,168 @@ export const oceanColor = '#71a0e9'
 
 const landColor = '#dbedb7',
 	residentialColor = 'hsl(54, 45%, 91%)'
+
+const terrainLayers = [
+	{
+		id: 'Hillshade',
+		type: 'hillshade',
+		source: 'gel-raster-dem',
+		layout: {
+			visibility: 'visible',
+		},
+		paint: {
+			'hillshade-accent-color': 'hsl(51, 30%, 79%)',
+			'hillshade-shadow-color': 'hsl(0, 0%, 50%)',
+			'hillshade-highlight-color': 'hsl(0, 0%, 83%)',
+		},
+	},
+	{
+		id: 'Contour index',
+		type: 'line',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			visibility: 'visible',
+		},
+		paint: {
+			'line-color': 'hsl(22, 35%, 55%)',
+			'line-width': 1.3,
+			'line-opacity': {
+				stops: [
+					[7, 0.2],
+					[10, 0.6],
+				],
+			},
+		},
+		filter: ['all', ['in', 'nth_line', 5, 10], ['!has', 'glacier']],
+	},
+	{
+		id: 'Glacier contour index',
+		type: 'line',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			visibility: 'visible',
+		},
+		paint: {
+			'line-color': 'hsl(192, 85%, 54%)',
+			'line-width': 1.3,
+			'line-opacity': {
+				stops: [
+					[7, 0.2],
+					[10, 0.6],
+				],
+			},
+		},
+		filter: ['all', ['in', 'nth_line', 5, 10], ['==', 'glacier', 1]],
+	},
+	{
+		id: 'Contour',
+		type: 'line',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			visibility: 'visible',
+		},
+		paint: {
+			'line-color': 'hsl(22, 35%, 55%)',
+			'line-width': 0.8,
+			'line-opacity': 0.5,
+		},
+		filter: ['all', ['!in', 'nth_line', 5, 10], ['!has', 'glacier']],
+	},
+	{
+		id: 'Glacier contour',
+		type: 'line',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			visibility: 'visible',
+		},
+		paint: {
+			'line-color': 'hsl(192, 85%, 54%)',
+			'line-width': 0.8,
+			'line-opacity': 0.5,
+		},
+		filter: ['all', ['!in', 'nth_line', 5, 10], ['==', 'glacier', 1]],
+	},
+	{
+		id: 'Contour labels',
+		type: 'symbol',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			'text-font': ['Roboto Regular', 'Noto Sans Regular'],
+			'text-size': {
+				base: 1,
+				stops: [
+					[15, 9.5],
+					[20, 12],
+				],
+			},
+			'text-field': '{height}',
+			visibility: 'visible',
+			'text-padding': 10,
+			'symbol-placement': 'line',
+			'symbol-avoid-edges': true,
+			'text-allow-overlap': false,
+			'text-ignore-placement': false,
+			'text-rotation-alignment': 'map',
+		},
+		paint: {
+			'text-color': 'hsl(20, 28%, 32%)',
+			'text-halo-blur': 1,
+			'text-halo-color': 'hsl(0, 0%, 100%)',
+			'text-halo-width': 0.5,
+		},
+		metadata: {},
+		filter: [
+			'all',
+			['==', '$type', 'LineString'],
+			['in', 'nth_line', 10, 5],
+			['>', 'height', 0],
+			['!=', 'glacier', 1],
+		],
+	},
+	{
+		id: 'Glacier contour labels',
+		type: 'symbol',
+		source: 'contours',
+		'source-layer': 'contours',
+		layout: {
+			'text-font': ['Roboto Regular', 'Noto Sans Regular'],
+			'text-size': {
+				base: 1,
+				stops: [
+					[15, 9.5],
+					[20, 12],
+				],
+			},
+			'text-field': '{height}',
+			visibility: 'visible',
+			'text-padding': 10,
+			'symbol-placement': 'line',
+			'symbol-avoid-edges': true,
+			'text-allow-overlap': false,
+			'text-ignore-placement': false,
+			'text-rotation-alignment': 'map',
+		},
+		paint: {
+			'text-color': 'hsl(192, 85%, 38%)',
+			'text-halo-blur': 1,
+			'text-halo-color': 'hsl(0,0%,100%)',
+			'text-halo-width': 0.5,
+		},
+		metadata: {},
+		filter: [
+			'all',
+			['==', '$type', 'LineString'],
+			['in', 'nth_line', 10, 5],
+			['>', 'height', 0],
+			['==', 'glacier', 1],
+		],
+	},
+]
 
 const layers = [
 	{
@@ -376,19 +542,7 @@ const layers = [
 		metadata: {},
 		filter: ['in', 'class', 'wetland'],
 	},
-	{
-		id: 'Hillshade',
-		type: 'hillshade',
-		source: 'gel-raster-dem',
-		layout: {
-			visibility: 'visible',
-		},
-		paint: {
-			'hillshade-accent-color': 'hsl(51, 30%, 79%)',
-			'hillshade-shadow-color': 'hsl(0, 0%, 50%)',
-			'hillshade-highlight-color': 'hsl(0, 0%, 83%)',
-		},
-	},
+	...terrainLayers,
 	{
 		id: 'Industrial',
 		type: 'fill',
