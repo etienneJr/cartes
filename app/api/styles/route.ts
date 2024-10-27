090c5e2d5ff4801ddf41cd3a3001ab2eb290e0c6
Mael <laem@kont.me>
2024-10-27 12:16:07 +0100

    En-ête CORS pour charger le style dans Maputnik

--- app/api/styles/route.ts
+++ app/api/styles/route.ts
@@ -1,5 +1,11 @@
 import franceStyle from '@/app/styles/france'
 
 export async function GET(request: Request) {
-	return Response.json(franceStyle(false))
+	return Response.json(franceStyle(false), {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		},
+	})
 }
