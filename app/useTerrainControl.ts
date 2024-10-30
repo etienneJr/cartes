c2a1c969565396a031f3a671bbac39fe7d82b9a9
Mael <laem@kont.me>
2024-10-30 18:27:47 +0100

    Désactivation du mode globe quand mode relief
    On verra par la suite pour le réactiver quand ça fonctionnera sur
    MapLibre :)
    
    Fixes #643

--- app/useTerrainControl.ts
+++ app/useTerrainControl.ts
@@ -1,19 +1,30 @@
 import maplibregl from 'maplibre-gl'
 import { useEffect } from 'react'
+import { defaultProjection } from './effects/useAddMap'
 
 export default function useTerrainControl(map, style) {
 	useEffect(() => {
 		if (!map) return
 		if (!style.hasTerrain) return
+
 		const control = new maplibregl.TerrainControl({
 			source: 'terrain-rgb',
 			exaggeration: 1,
 		})
 
+		const onTerrain = (e) => {
+			console.log('Terrain activé ou désactivé', e)
+
+			if (e.terrain != null) map.setProjection({ type: 'mercator' })
+			else map.setProjection(defaultProjection)
+		}
+		map.on('terrain', onTerrain)
+
 		map.addControl(control, 'top-right')
 		return () => {
 			try {
 				control && map.removeControl(control)
+				map.off(onTerrain)
 			} catch (e) {
 				console.log('Trying to remove terrain 3D control failed', e)
 			}
