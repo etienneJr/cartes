7d5b49d28606eda98a77262cd9dc068a15082596
Mael <laem@kont.me>
2024-11-03 12:07:17 +0100

    Pas d'image venant de og:image si erreur
    Example : http://localhost:8080/?allez=Arm%C3%A9nie|r364066|44.6737|40.7696#7.04/40.042/43.914

--- app/Content.tsx
+++ app/Content.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@/components/loader'
 import useSetSearchParams from '@/components/useSetSearchParams'
 import { getThumb } from '@/components/wikidata'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 import BookmarkButton from './BookmarkButton'
 import Bookmarks from './Bookmarks'
@@ -29,6 +29,7 @@ import { defaultTransitFilter } from './transport/TransitFilter'
 import TransportMap from './transport/TransportMap'
 import useOgImageFetcher from './useOgImageFetcher'
 import { useWhatChanged } from '@/components/utils/useWhatChanged'
+import { feature } from '@turf/turf'
 
 const getMinimumQuickSearchZoom = (mobile) => (mobile ? 10.5 : 12) // On a small screen, 70 %  of the tiles are not visible, hence this rule
 
@@ -76,6 +77,8 @@ export default function Content(props) {
 		ogImage = ogImages[url],
 		tagImage = tags?.image,
 		mainImage = tagImage || ogImage // makes a useless request for ogImage that won't be displayed to prefer mainImage : TODO also display OG
+
+	const [featureImageError, setFeatureImageError] = useState(false)
 
 	const [tutorials, setTutorials] = useLocalStorage('tutorials', {})
 	const introductionRead = tutorials.introduction,
@@ -254,9 +257,10 @@ export default function Content(props) {
 								}}
 							/>
 						)}
-						{mainImage && (
+						{mainImage && !featureImageError && (
 							<FeatureImage
 								src={mainImage}
+								onError={() => setFeatureImageError(true)}
 								css={`
 									width: 100%;
 									height: 6rem;
