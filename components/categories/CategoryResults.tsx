4efd93608969927b647c69bd41aea808ea438a82
Mael <laem@kont.me>
2024-11-20 12:15:59 +0100

    WIP affichage sommaire des résultats de catégorie

--- components/categories/CategoryResults.tsx
+++ components/categories/CategoryResults.tsx
@@ -5,32 +5,36 @@ import closeIcon from '@/public/close.svg'
 
 export default function CategoryResults({ resultsEntries }) {
 	const setSearchParams = useSetSearchParams()
+	const results = resultsEntries.map(([k,v])=> {})
 	return (
-		<section
-			css={`
-				display: flex;
-				justify-content: space-between;
-				img {
-					width: 0.9rem;
-					height: auto;
-				}
-			`}
-		>
-			<div>
-				{resultsEntries.map(([k, v], i) => (
-					<>
-						<span>
-							<span>{v.length}</span> <span>{k.toLowerCase()}</span>
-						</span>
-						{i < resultsEntries.length - 1 && ', '}
-					</>
-				))}
+		<section css={``}>
+			<div
+				css={`
+					display: flex;
+					justify-content: space-between;
+					img {
+						width: 0.9rem;
+						height: auto;
+					}
+				`}
+			>
+				<div>
+					{resultsEntries.map(([k, v], i) => (
+						<>
+							<span>
+								<span>{v.length}</span> <span>{k.toLowerCase()}</span>
+							</span>
+							{i < resultsEntries.length - 1 && ', '}
+						</>
+					))}
+				</div>
+				{resultsEntries.length > 0 && (
+					<Link href={setSearchParams({ cat: undefined }, true)}>
+						<Image src={closeIcon} alt="Fermer" />
+					</Link>
+				)}
 			</div>
-			{resultsEntries.length > 0 && (
-				<Link href={setSearchParams({ cat: undefined }, true)}>
-					<Image src={closeIcon} alt="Fermer" />
-				</Link>
-			)}
+			<ol>{results.map(result)=> <li key={result.id}>{result.tags?.name}</li>}</ol>
 		</section>
 	)
 }
