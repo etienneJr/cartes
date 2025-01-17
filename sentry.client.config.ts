7b98cb58e8309cfd85801c8b7a1227ff0cc0338f
Mael <laem@kont.me>
2025-01-17 23:10:25 +0100

    J'arrive pas à bun build glitchtip

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-	dsn: 'http://983f777ba77848138577dff8df83c8be@pb.cartes.app/2',
+	dsn: 'https://983f777ba77848138577dff8df83c8be@pb.cartes.app/2',
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
