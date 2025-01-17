ad6d97f4c3772fe46a26dc49e73606a71518daae
Mael <laem@kont.me>
2025-01-17 22:26:44 +0100

    Configuration de sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import {withSentryConfig} from '@sentry/nextjs';
 import { withContentlayer } from 'next-contentlayer2'
 import createMDX from '@next/mdx'
 import path from 'path'
@@ -13,106 +14,140 @@ const __dirname = path.dirname(__filename)
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	reactStrictMode: false,
-	experimental: {
-		reactCompiler: true,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'avatars.githubusercontent.com',
-			},
-		],
-	},
-	/*
-	compilerOptions: {
-		baseUrl: '.',
-	},
-	*/
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		ignoreBuildErrors: true,
-	},
-	async rewrites() {
-		return [
-			{ source: '/feed.xml', destination: '/_next/static/feed.xml' },
-			{ source: '/lieu', destination: '/' },
-		]
-	},
-	async redirects() {
-		return [
-			{
-				source: '/prez',
-				destination: '/presentation/state-of-the-map-2024',
-				permanent: false,
-			},
-			{
-				source: '/nord',
-				destination: '/boussole',
-				permanent: false,
-			},
-			{
-				source: '/elections-legislatives-2024',
-				destination: '/?style=elections',
-				permanent: false,
-			},
-			{
-				source: '/elections-legislatives-2024/premier-tour',
-				destination: '/?style=elections',
-				permanent: false,
-			},
-		]
-	},
-	webpack: (config, options) => {
-		config.module.rules.push({
-			test: /\.ya?ml$/,
-			use: 'yaml-loader',
-		})
-		config.module.rules.push({ test: /\.mp3$/, type: 'asset/resource' })
-		config.module.rules.push({
-			test: /\.csv$/,
-			loader: 'csv-loader',
-			options: {
-				dynamicTyping: true,
-				header: true,
-				skipEmptyLines: true,
-			},
-		})
+				reactStrictMode: false,
+				experimental: {
+								reactCompiler: true,
+				},
+				images: {
+								remotePatterns: [
+												{
+																protocol: 'https',
+																hostname: 'avatars.githubusercontent.com',
+												},
+								],
+				},
+				/*
+				compilerOptions: {
+								baseUrl: '.',
+				},
+				*/
+				eslint: {
+								// Warning: This allows production builds to successfully complete even if
+								// your project has ESLint errors.
+								ignoreDuringBuilds: true,
+				},
+				typescript: {
+								// !! WARN !!
+								// Dangerously allow production builds to successfully complete even if
+								// your project has type errors.
+								// !! WARN !!
+								ignoreBuildErrors: true,
+				},
+				async rewrites() {
+								return [
+												{ source: '/feed.xml', destination: '/_next/static/feed.xml' },
+												{ source: '/lieu', destination: '/' },
+								]
+				},
+				async redirects() {
+								return [
+												{
+																source: '/prez',
+																destination: '/presentation/state-of-the-map-2024',
+																permanent: false,
+												},
+												{
+																source: '/nord',
+																destination: '/boussole',
+																permanent: false,
+												},
+												{
+																source: '/elections-legislatives-2024',
+																destination: '/?style=elections',
+																permanent: false,
+												},
+												{
+																source: '/elections-legislatives-2024/premier-tour',
+																destination: '/?style=elections',
+																permanent: false,
+												},
+								]
+				},
+				webpack: (config, options) => {
+								config.module.rules.push({
+												test: /\.ya?ml$/,
+												use: 'yaml-loader',
+								})
+								config.module.rules.push({ test: /\.mp3$/, type: 'asset/resource' })
+								config.module.rules.push({
+												test: /\.csv$/,
+												loader: 'csv-loader',
+												options: {
+																dynamicTyping: true,
+																header: true,
+																skipEmptyLines: true,
+												},
+								})
 
-		config.resolve.alias = {
-			...config.resolve.alias,
-			//https://github.com/Turfjs/turf/issues/2200
-			rbush: path.resolve(__dirname, '/node_modules/rbush/rbush.js'),
-		}
+								config.resolve.alias = {
+												...config.resolve.alias,
+												//https://github.com/Turfjs/turf/issues/2200
+												rbush: path.resolve(__dirname, '/node_modules/rbush/rbush.js'),
+								}
 
-		config.plugins.push(
-			new CopyPlugin({
-				patterns: [
-					{
-						to: path.resolve(__dirname, 'public/indoorequal'),
-						from: path.resolve(
-							__dirname,
-							'node_modules/maplibre-gl-indoorequal/sprite'
-						),
-					},
-				],
-			})
-		)
+								config.plugins.push(
+												new CopyPlugin({
+																patterns: [
+																				{
+																								to: path.resolve(__dirname, 'public/indoorequal'),
+																								from: path.resolve(
+																												__dirname,
+																												'node_modules/maplibre-gl-indoorequal/sprite'
+																								),
+																				},
+																],
+												})
+								)
 
-		return config
-	},
-	pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+								return config
+				},
+				pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
 const withMDX = createMDX({ options: mdxOptions })
 
-export default withYak(withContentlayer(withMDX(nextConfig)))
+export default withSentryConfig(withYak(withContentlayer(withMDX(nextConfig))), {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+org: "cartes",
+project: "cartes-web",
+sentryUrl: "https://pb.cartes.app/",
+
+// Only print logs for uploading source maps in CI
+silent: !process.env.CI,
+
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// This can increase your server load as well as your hosting bill.
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+	dsn: 'https://3b048e28d2ce4b6192bcfeb6a9dae45b@pb.cartes.app/1',
+
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
+})
