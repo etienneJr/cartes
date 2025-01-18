0272dae96e9f7506a8d10c4f6ea1bb657f039b12
Mael <laem@kont.me>
2025-01-18 17:16:21 +0100

    Fichier qui créée l'annuaire des communes d'un département

---
+++ lib/annuaireScope.ts
@@ -0,0 +1,20 @@
+import fs from 'fs'
+
+const doFetch = async (departementCode = 35) => {
+	const request = await fetch(
+		`https://geo.api.gouv.fr/departements/${departementCode}/communes`
+	)
+	const json = await request.json()
+
+	const big = json.filter((commune) => commune.population > 10000)
+
+	const sorted = big.sort((b, a) => a.population - b.population)
+	console.log(sorted)
+	fs.writeFileSync(
+		'public/communes-' + departementCode + '.json',
+		JSON.stringify(sorted)
+	)
+	return sorted
+}
+
+doFetch()
