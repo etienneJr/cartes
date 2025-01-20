b30ebc13546673abc0b2defc5e17dbc956457923
Mael <laem@kont.me>
2025-01-20 10:16:04 +0100

    Texte sur l'annuaire

--- app/lieux/Régions.tsx
+++ app/lieux/Régions.tsx
@@ -15,6 +15,18 @@ export default function Régions() {
 	return (
 		<main>
 			<h1>Annuaire des régions de France</h1>
+			<p>
+				En complément de la vue cartographique, explorez les lieux de France
+				(sites naturels, services publics, commerces... ) région par région,
+				puis département par département.
+			</p>
+			<p>
+				Vous aurez ici accès à toute la richesse de la carte ouverte{' '}
+				<a href="openstreetmap.fr">OpenStreetMap</a>, une carte contributive où
+				chacun, du responsable d'un commerce au simple habitant d'un quartier
+				peut enrichir les horaires, la description et tous les attributs d'un
+				lieu.
+			</p>
 			<ol>
 				{entries.map(([code, list]) => {
 					const name = list[0].nom_region
