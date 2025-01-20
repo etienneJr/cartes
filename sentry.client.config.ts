7e35ab51adb065390cde463614e2da74bd017245
Mael <laem@kont.me>
2025-01-20 11:09:38 +0100

    Pas sentry en localhost

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,11 +2,12 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://06122d20c42fb7fbece2556cbc712f26@o4508661291679744.ingest.de.sentry.io/4508661293514832",
+	dsn: 'https://06122d20c42fb7fbece2556cbc712f26@o4508661291679744.ingest.de.sentry.io/4508661293514832',
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
+	enabled: process.env.NODE_ENV !== 'development',
+})
