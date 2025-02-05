e471192de46dabf295e250d0f9c9c66f97604cd3
Mael <laem@kont.me>
2025-02-05 19:24:33 +0100

    Amélioration de l'icône géoloc'

--- app/PlaceSearch/_components/Geolocate.tsx
+++ app/PlaceSearch/_components/Geolocate.tsx
@@ -1,24 +1,33 @@
 import useSetSearchParams from '@/components/useSetSearchParams'
-import { css } from 'next-yak'
+import Image from 'next/image'
+import geolocateIcon from '@/public/geolocate.svg'
+import { styled } from 'next-yak'
 
 export function Geolocate() {
 	const setSearchParams = useSetSearchParams()
 	return (
-		<button onClick={() => setSearchParams({ geoloc: 'oui' })}>
-			<span
-				css={css`
-					background-position: 50%;
-					background-repeat: no-repeat;
-					display: block;
-					display: inline-block;
-					width: 1rem;
-					height: 1rem;
-					margin-right: 0.4rem;
-					vertical-align: sub;
-					background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='29' height='29' fill='%23333' viewBox='0 0 20 20'%3E%3Cpath d='M10 4C9 4 9 5 9 5v.1A5 5 0 0 0 5.1 9H5s-1 0-1 1 1 1 1 1h.1A5 5 0 0 0 9 14.9v.1s0 1 1 1 1-1 1-1v-.1a5 5 0 0 0 3.9-3.9h.1s1 0 1-1-1-1-1-1h-.1A5 5 0 0 0 11 5.1V5s0-1-1-1m0 2.5a3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 1 0-7'/%3E%3Ccircle cx='10' cy='10' r='2'/%3E%3C/svg%3E");
-				`}
+		<Button onClick={() => setSearchParams({ geoloc: 'oui' })}>
+			<Image
+				src={geolocateIcon}
+				alt="Icône représentant la géolocalisation de l'utilisateur"
 			/>
 			Me géolocaliser
-		</button>
+		</Button>
 	)
 }
+
+const Button = styled.button`
+	display: flex;
+	padding: 0;
+	margin: 0 auto;
+	text-decoration: underline;
+	text-decoration-color: var(--lightColor);
+	text-decoration-thickness: 2px;
+
+	align-items: center;
+	> img {
+		width: 2rem;
+		height: 2rem;
+		border-radius: 0.6rem;
+	}
+`
